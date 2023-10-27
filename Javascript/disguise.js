0ecf8128bc053ed70bf37c6ce25b8fc938d0a884
Branson <104116765+5uggz@users.noreply.github.com>
2023-10-26 21:21:40 -0400

    Create disguise.js

---
+++ Javascript/disguise.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('changeFaviconTitle');
+        const pageTitle = document.getElementById('pageTitle');
+        const favicon = document.getElementById('favicon');
+
+        button.addEventListener('click', () => {
+            // Change the title
+            pageTitle.textContent = 'Canvas';
+
+            // Change the favicon
+            favicon.href = 'Images/canvas.ico';
+        });
