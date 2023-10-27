df46ba9dd907506fed7f86bbd01d8d3c133c8461
Branson <104116765+5uggz@users.noreply.github.com>
2023-10-26 21:39:20 -0400

    Update disguise.js

--- Javascript/disguise.js
+++ Javascript/disguise.js
@@ -1,11 +1,12 @@
+// This is the content of "disguise.js"
 const button = document.getElementById('changeFaviconTitle');
-        const pageTitle = document.getElementById('pageTitle');
-        const favicon = document.getElementById('favicon');
+const pageTitle = document.getElementById('pageTitle');
+const favicon = document.getElementById('favicon');
 
-        button.addEventListener('click', () => {
-            // Change the title
-            pageTitle.textContent = 'Canvas';
+button.addEventListener('click', () => {
+    // Change the title
+    pageTitle.textContent = 'Canvas';
 
-            // Change the favicon
-            favicon.href = 'Images/canvas.ico';
-        });
+    // Change the favicon
+    favicon.href = 'Images/canvas.ico';
+});
